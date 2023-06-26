995bcfc0f6280c6eba31f02528ca002509336b97
angkamilama <angkami46@gmail.com>
2023-06-26 10:13:43 +0200

    add conditiional rendering

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,17 @@
 import style from "./Dashboard.modules.css";
+import { useState } from "react";
 
 function Dashboard({ user }) {
-  console.log(user);
   return (
     <div className="logged-msg">
-      <h4>
-        Congratulation !!! You have been logged in as{" "}
-        <span className="user">{user}</span>
-      </h4>
+      {user ? (
+        <h4>
+          Congratulation !!! You have been logged in as{" "}
+          <span className="user">{user}</span>
+        </h4>
+      ) : (
+        " Sorry! You have not been logged in."
+      )}
     </div>
   );
 }
